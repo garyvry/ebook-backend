d81801410c909cf0cf7cc9a2aded7600858f70b1
garyvry@outlook.com <garyvry@outlook.com>
2019-12-13 22:17:49 +0000

    testing continued

--- src/parser/epub/epub.js
+++ src/parser/epub/epub.js
@@ -1,26 +1,54 @@
 const EPub = require('epub')
 
-const parse = (path) => {
-  console.log('epub parse: ' + path)
+const scanDirectory = async (path, recurrsive) => {
+  return new Promise(
+    async (resolve, reject) => {
+      console.log('scanDirectory')
 
-  var epub = new EPub(path)
-
-  epub.on('end', function () {
-    // epub is now usable
-    console.log(epub.metadata)
-    // epub.getChapter('chapter_id', function (err, text) {
-    //   if (err) {
-    //     console.log(err)
-    //   } else {
-    //     console.log(text)
-    //   }
-    // })
-  })
-  epub.parse()
+      resolve({})
+    }
+  )
 }
 
-parse('./src/books/Kubernetes_Up_and_Running.epub')
+const parse = async path => {
+  return new Promise(
+    async (resolve, reject) => {
+      console.log('epub parse: ' + path)
+      var epub = new EPub(path)
+      epub.on('end', () => {
+        // epub is now usable
+        console.log(epub.metadata)
+        // epub.getChapter('chapter_id', function (err, text) {
+        //   if (err) {
+        //     console.log(err)
+        //   } else {
+        //     console.log(text)
+        //   }
+        // })
+      })
+      epub.parse()
+      resolve({})
+    }
+  )
+}
 
-// module.exports = {
-//   parse
-// }
+const getMetadata = async path => {
+  return new Promise(
+    async (resolve, reject) => {
+      console.log('getMetadata: ' + path)
+      var epub = new EPub(path)
+      epub.on('end', () => {
+        console.log(epub.metadata)
+        resolve(epub.metadata)
+      })
+      epub.parse()
+    }
+  )
+}
+
+getMetadata('./src/books/Kubernetes_Up_and_Running.epub')
+
+module.exports = {
+  getMetadata,
+  scanDirectory
+}
